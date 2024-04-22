3fd7b576a8295a8574d36a4348c24ff33f629811
박지영 <jiyoungvvvvv@gmail.com>
2024-04-22 10:15:50 +0900

    Add: vaiv 생변소 omw용

---
+++ test/omw-lifechange/asset/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('.preview-modal .modal-slide').slick({
+    infinite: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+  $('.preview-modal .modal-slide .slick-arrow').html('');
+  $('.preview-modal .modal-slide .slick-dots li').html('');
+
+  $('.preview-modal .modal-next-button').on('click', function () {
+    $('.preview-modal .modal-slide').slick('slickGoTo', 1);
+  });
+});
